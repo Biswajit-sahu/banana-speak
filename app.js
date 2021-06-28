24c8a899007b21ba6d332bf9f07b640db0979a51
Biswajit-sahu <sahubiswajit478@gmail.com>
2021-06-28 09:55:31 +0530

    added minion api

--- app.js
+++ app.js
@@ -3,10 +3,30 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
 
 function clickHandler() {
-    outputDiv.innerText = "anything "  + txtInput.value;
-};
+    var inputText = txtInput.value; // taking input
 
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+        })
+        .catch(errorHandler)
+};
 
 btnTranslate.addEventListener("click", clickHandler)
